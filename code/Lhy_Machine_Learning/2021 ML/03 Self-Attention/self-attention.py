628763089a85dbf69c3949daf131215b1fd427a9
ziguiway <ziguiway@163.com>
2026-07-14 11:05:18 +0800

    vault backup: 2026-07-14 11:05:18

--- code/Lhy_Machine_Learning/2021 ML/03 Self-Attention/self-attention.py
+++ code/Lhy_Machine_Learning/2021 ML/03 Self-Attention/self-attention.py
@@ -1,11 +1,17 @@
 import numpy as np
 
 
+def softmax(x):
+    exp_x = np.exp(x)
+    return exp_x / np.sum(exp_x)
+
 def self_attention(X,W_Q,W_K,W_V):
     Q = W_Q @ X
     K = W_k @ X
     V = W_V @ X
 
     attention_scroce = K.T @ Q
+
+    attention_scroce = softmax(attention_scroce)
+
     
-     
