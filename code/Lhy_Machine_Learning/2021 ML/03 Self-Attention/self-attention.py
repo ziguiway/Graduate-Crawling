7fd8383c0f1b91c6e0fb05b73043911a34179ef2
ziguiway <ziguiway@163.com>
2026-07-14 11:03:11 +0800

    vault backup: 2026-07-14 11:03:11

--- code/Lhy_Machine_Learning/2021 ML/03 Self-Attention/self-attention.py
+++ code/Lhy_Machine_Learning/2021 ML/03 Self-Attention/self-attention.py
@@ -1,4 +1,11 @@
 import numpy as np
 
 
-def self_attention(X,Q_)
+def self_attention(X,W_Q,W_K,W_V):
+    Q = W_Q @ X
+    K = W_k @ X
+    V = W_V @ X
+
+    attention_scroce = K.T @ Q
+    
+     
