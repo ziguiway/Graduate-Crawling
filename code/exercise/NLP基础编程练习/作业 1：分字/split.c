b8e6462f221a7c79e34e6222c6d2196ed7eed26b
ziguiway <ziguiway@163.com>
2026-07-16 12:58:10 +0800

    vault backup: 2026-07-16 12:58:10

--- code/exercise/NLP基础编程练习/作业 1：分字/split.c
+++ code/exercise/NLP基础编程练习/作业 1：分字/split.c
@@ -3,8 +3,18 @@
 
 int main()
 {
-    
+    FILE *fp;
+    fp = fopen("./data/Sentence.txt", "rb");
+    if(fp == NULL)
+    {
+        printf("Error: File not found.\n");
+        return 1;
+    }
+
+    int ch = fgetc(fp);
+    printf("%c", ch);
+
+
+    fclose(fp);
     return 0;
 }
-
-
