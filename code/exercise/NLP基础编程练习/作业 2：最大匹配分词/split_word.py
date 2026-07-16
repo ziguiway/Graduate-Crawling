964f52ce19d61566ae931651763266d307b99502
ziguiway <ziguiway@163.com>
2026-07-16 17:20:01 +0800

    vault backup: 2026-07-16 17:20:01

--- code/exercise/NLP基础编程练习/作业 2：最大匹配分词/split_word.py
+++ code/exercise/NLP基础编程练习/作业 2：最大匹配分词/split_word.py
@@ -1,6 +1,6 @@
 def lode_file(file_path:str) -> list:
     with open(file_path,'r') as fp:
-        lines = [line.strip() for line in fp.readlines()]
+        lines = [line.strip("\n") for line in fp.readlines()]
     return lines
 
 
@@ -11,9 +11,11 @@ def get_dic_max_len(word_list:list) -> int:
             max_len = max(max_len, len(word))
     return max_len
 
-def split_word(text:str, word_list:list) -> list:
+def split_word(word_list:list, text:str) -> list:
+    result = []
     max_len = get_dic_max_len(word_list)
-    
+    while text_len = len(text) > 0:
+
 
 
 
