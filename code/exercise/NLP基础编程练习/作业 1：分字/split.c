f26843a2201e4e11cf56839dea08dda6d47ceb84
ziguiway <ziguiway@163.com>
2026-07-16 13:23:36 +0800

    vault backup: 2026-07-16 13:23:36

--- code/exercise/NLP基础编程练习/作业 1：分字/split.c
+++ code/exercise/NLP基础编程练习/作业 1：分字/split.c
@@ -9,33 +9,46 @@ void byte_to_bits(int ch, int bits[8]) {
     }
 }
 
-
+int scan(int bits[8]) {
+    int count = 1;
+    for(int i = 0; i < 7; i++)
+    {
+        if(bits[i] == 1 && bits[i+1] == 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 int main()
 {
-    FILE *fp;
-    fp = fopen("./data/Sentence.txt", "rb");
-    if(fp == NULL)
-    {
-        printf("Error: File not found.\n");
-        return 1;
-    }
+    // FILE *fp;
+    // fp = fopen("./data/Sentence.txt", "rb");
+    // if(fp == NULL)
+    // {
+    //     printf("Error: File not found.\n");
+    //     return 1;
+    // }
 
-    int ch = fgetc(fp);
+    // int ch = fgetc(fp);
     
-    while(ch != EOF)
-    {
-        int bits[8];
-        byte_to_bits(ch, bits);
-        for(int i = 0; i < 8; i++)
-        {
-            printf("%d", bits[i]);
-        }
-        printf("\n");
-        ch = fgetc(fp);
-    }
+    // while(ch != EOF)
+    // {
+    //     int bits[8];
+    //     byte_to_bits(ch, bits);
+    //     for(int i = 0; i < 8; i++)
+    //     {
+    //         printf("%d", bits[i]);
+    //     }
+    //     printf("\n");
+    //     ch = fgetc(fp);
+    // }
 
 
-    fclose(fp);
+    // fclose(fp);
+
+    int a[8] = {1, 0, 1, 1, 0, 1, 1, 0};
+    printf("%d", scan(a));
     return 0;
 }
