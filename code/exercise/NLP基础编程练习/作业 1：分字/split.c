f3cf00965993f526c70900af7d923bc4b9625f9b
ziguiway <ziguiway@163.com>
2026-07-16 15:57:17 +0800

    vault backup: 2026-07-16 15:57:17

--- code/exercise/NLP基础编程练习/作业 1：分字/split.c
+++ code/exercise/NLP基础编程练习/作业 1：分字/split.c
@@ -65,6 +65,7 @@ int main()
         // 4. 跳过当前字符占的字节
         if(skip_count > 0)
         {
+            char_bytes[char_len++] = ch;
             
             ch = fgetc(fp);
             skip_count--;
