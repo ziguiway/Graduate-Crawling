883a086439e520392bed6be63fd64a7cdf340949
ziguiway <ziguiway@163.com>
2026-07-21 11:32:09 +0800

    vault backup: 2026-07-21 11:32:09

--- code/exercise/NLP基础编程练习/作业 3：有监督与无监督 HMM 词性标注/supervised_hmm.py
+++ code/exercise/NLP基础编程练习/作业 3：有监督与无监督 HMM 词性标注/supervised_hmm.py
@@ -1,10 +1,21 @@
 def load_conll(file_path: str) -> dict:
-    
+    data = []
     with open(file_path, 'r') as fp:
-        for line in fp:
-            line = line.strip()
-            if not line:
-                continue
-            line = line.split("")
+        lines = fp.readlines()
 
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        line = line.split("\t")
+        tmp = {
+            id: line[0],
+            word: line[1],
+            pos: line[2],
+            tag: line[3],
+        }
+        data.append(tmp)
     
+    return data
+
+load_conll("data/train.conll")
